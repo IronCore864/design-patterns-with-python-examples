06644035f054aee5e42a58328cb44037df82df0e
Tiexin Guo <guotiexin@gmail.com>
2021-08-30 15:53:04 +0800

    update

--- 3_behavioral/iterator/README.md
+++ 3_behavioral/iterator/README.md
@@ -1,5 +1,9 @@
 Iterator is a behavioral design pattern that lets you traverse elements of a collection without exposing its underlying representation (list, stack, tree, etc.).
 
+![](iterator1.png)
+
+![](iterator2.png)
+
 - Use the Iterator pattern when your collection has a complex data structure under the hood, but you want to hide its complexity from clients (either for convenience or security reasons).
 - Use the pattern to reduce duplication of the traversal code across your app.
 - Use the Iterator when you want your code to be able to traverse different data structures or when types of these structures are unknown beforehand.
--- 3_behavioral/iterator/main.py
+++ 3_behavioral/iterator/main.py
@@ -1,50 +1,105 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
 from collections.abc import Iterable, Iterator
 from typing import Any, List
 
 
-class AlphabeticalOrderIterator(Iterator):
-    _position: int = None
-    _reverse: bool = False
+class SocialNetwork(ABC):
+    @abstractmethod
+    def create_friends_iterator(self, facebook, profile_id):
+        pass
 
-    def __init__(self, collection: WordsCollection, reverse: bool = False) -> None:
-        self._collection = collection
-        self._reverse = reverse
-        self._position = -1 if reverse else 0
-
-    def __next__(self):
-        try:
-            value = self._collection[self._position]
-            self._position += -1 if self._reverse else 1
-        except IndexError:
-            raise StopIteration()
-
-        return value
+    @abstractmethod
+    def create_coworkers_iterator(self, facebook, profile_id):
+        pass
 
 
-class WordsCollection(Iterable):
-    def __init__(self) -> None:
-        self._collection = []
+class Facebook(SocialNetwork):
+    def create_friends_iterator(self, profile_id):
+        return FacebookIterator(self, profile_id, "friends")
 
-    def __iter__(self) -> AlphabeticalOrderIterator:
-        return AlphabeticalOrderIterator(self._collection)
+    def create_coworkers_iterator(self, profile_id):
+        return FacebookIterator(self, profile_id, "coworkers")
 
-    def get_reverse_iterator(self) -> AlphabeticalOrderIterator:
-        return AlphabeticalOrderIterator(self._collection, True)
+    def social_graph_request(self, profile_id, type):
+        # mock
+        if type == 'friends':
+            return [
+                Profile("FriendA", "a@gmail.com"),
+                Profile("FriendB", "b@gmail.com"),
+                Profile("FriendC", "c@gmail.com"),
+                Profile("FriendD", "d@gmail.com")
+            ]
+        return [
+            Profile("Coworker1", "1@gmail.com"),
+            Profile("Coworker2", "2@gmail.com"),
+            Profile("Coworker3", "3@gmail.com"),
+            Profile("Coworker4", "4@gmail.com")
+        ]
 
-    def add_item(self, item: Any):
-        self._collection.append(item)
+
+class Profile():
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def get_email(self):
+        return self.email
+
+
+class ProfileIterator(ABC):
+    @abstractmethod
+    def get_next(self):
+        pass
+
+    @abstractmethod
+    def has_more(self):
+        pass
+
+
+class FacebookIterator(ProfileIterator):
+    def __init__(self, facebook, profile_id, type):
+        self.facebook = facebook
+        self.profile_id = profile_id
+        self.type = type
+        self.current_position = 0
+        self.cache = None
+
+    def __lazy_init(self):
+        if not self.cache:
+            self.cache = self.facebook.social_graph_request(
+                self.profile_id,
+                self.type
+            )
+
+    def get_next(self):
+        res = self.cache[self.current_position]
+        if self.has_more():
+            self.current_position += 1
+        return res
+
+    def has_more(self):
+        self.__lazy_init()
+        return self.current_position < len(self.cache)
+
+
+class SocialSpammer:
+    def send(self, iterator: ProfileIterator, message: str):
+        while iterator.has_more():
+            profile = iterator.get_next()
+            print(f'{message} sent to {profile.get_email()}')
+
+
+def client_code():
+    network = Facebook()
+    spammer = SocialSpammer()
+
+    iterator = network.create_friends_iterator('fake_profile_id')
+    spammer.send(iterator, "Very important message")
+
+    iterator = network.create_coworkers_iterator('fake_profile_id')
+    spammer.send(iterator, "Very important message")
 
 
 if __name__ == "__main__":
-    collection = WordsCollection()
-    collection.add_item("First")
-    collection.add_item("Second")
-    collection.add_item("Third")
-
-    print("Straight traversal:")
-    print("\n".join(collection))
-    print("")
-
-    print("Reverse traversal:")
-    print("\n".join(collection.get_reverse_iterator()), end="")
+    client_code()
