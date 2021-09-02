708a147141362287afa047af33528937c6a16acb
Tiexin Guo <guotiexin@gmail.com>
2021-09-02 10:10:54 +0800

    update decorator

--- 2_structrual/decorator/README.md
+++ 2_structrual/decorator/README.md
@@ -1,4 +1,8 @@
 Decorator is a structural design pattern that lets you attach new behaviors to objects by placing these objects inside special wrapper objects that contain the behaviors.
 
+![](decorator1.png)
+
+![](decorator2.png)
+
 - Use the Decorator pattern when you need to be able to assign extra behaviors to objects at runtime without breaking the code that uses these objects.
 - Use the pattern when it's awkward or not possible to extend an object's behavior using inheritance.
--- 2_structrual/decorator/main.py
+++ 2_structrual/decorator/main.py
@@ -1,35 +1,84 @@
-class Notifier():
-    def notify(self):
+from abc import ABC, abstractmethod
+
+
+class DataSource(ABC):
+    @abstractmethod
+    def write_data(self, data):
+        pass
+
+    @abstractmethod
+    def read_data(self):
         pass
 
 
-class Decorator(Notifier):
-    _notifier: Notifier = None
+class FileDataSource(DataSource):
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = ""
 
-    def __init__(self, notifier: Notifier):
-        self._notifier = notifier
+    def write_data(self, data):
+        print(f'Writing data {data} to file {self.filename}')
+        self.data = data
 
-    def operation(self):
-        return self._notifier.notify()
-
-
-class SMSDecorator(Decorator):
-    def notify(self):
-        return "SMS notification sent!"
-
-
-class SlackDecorator(Decorator):
-    def notify(self):
-        return "Slack notification sent!"
+    def read_data(self):
+        print(f'Reading data from file {self.filename}')
+        return self.data
 
 
-def client_code(c: Notifier):
-    print(c.notify())
+class DataSourceDecorator(DataSource):
+    def __init__(self, source: DataSource):
+        self.wrapper = source
+
+    def write_data(self, data):
+        self.wrapper.write_data(data)
+
+    def read_data(self):
+        return self.wrapper.read_data()
+
+
+class EncryptionDecorator(DataSourceDecorator):
+    def write_data(self, data):
+        print(f'Encrypting data {data}...')
+        self.wrapper.write_data('Encrypted'+data)
+
+    def read_data(self):
+        data = self.wrapper.read_data()
+        print(f'Decrypting data...')
+        return data.lstrip('Encrypted')
+
+
+class CompressionDecorator(DataSourceDecorator):
+    def write_data(self, data):
+        print(f'Compressing data {data}...')
+        data = data[:len(data)//2]
+        self.wrapper.write_data(data)
+
+    def read_data(self):
+        data = self.wrapper.read_data()
+        print(f'Decompressing data...')
+        return data*2
+
+
+def client_code():
+    source = FileDataSource("somefile.dat")
+
+    source.write_data("aaa")
+    data = source.read_data()
+    print(f'Reading the data just written: {data}')
+    print()
+
+    source = EncryptionDecorator(source)
+    source.write_data("bbb")
+    data = source.read_data()
+    print(f'Reading the data just written: {data}')
+    print()
+
+    source = CompressionDecorator(source)
+    source.write_data("abcabc")
+    data = source.read_data()
+    print(f'Reading the data just written: {data}')
+    print()
 
 
 if __name__ == "__main__":
-    notifier = Notifier()
-    sms = SMSDecorator(notifier)
-    slack = SlackDecorator(notifier)
-    client_code(sms)
-    client_code(slack)
+    client_code()
